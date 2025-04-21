4c20ff175e7d68295f5b4ca54ea51356f67f3890
HR-04 <harinirengarajan203@gmail.com>
2025-04-22 02:17:19 +0530

    Refactor: Simplify next.config.mjs by removing output and images configuration; switch to ES module export

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true, // if you're having image optimization issues
-  },
-};
+const nextConfig = {};
 
-module.exports = nextConfig;
+export default nextConfig;
